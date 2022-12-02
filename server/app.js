bfbe3bae47f5cf934d791da2795c467f1d3006f0
Henry W <henry.woodmansee@gmail.com>
2022-12-01 19:37:39 -0500

    chungus

--- server/app.js
+++ server/app.js
@@ -1,41 +1,46 @@
 // Instantiate Express and the application
-const express = require('express');
+const express = require("express");
 const app = express();
 
 // Process environment variables
-require('dotenv').config();
+require("dotenv").config();
 
+const sqlite3 = require("sqlite3");
+const db = new sqlite3.Database(
+	process.env.DATA_SOURCE,
+	sqlite3.OPEN_READWRITE
+);
 // Express using json
 app.use(express.json());
 
 // Connect router for trees API
-const treesRouter = require('./routes/trees');
-app.use('/trees', treesRouter);
+const treesRouter = require("./routes/trees");
+app.use("/trees", treesRouter);
 
 // Root route
-app.get('/', (req, res) => {
-    res.json({
-        message: "API server is running"
-    });
+app.get("/", (req, res) => {
+	res.json({
+		message: "API server is running"
+	});
 });
 
 // Custom error middleware (triggered via call to next(err))
 app.use((err, req, res, next) => {
-    console.error(err);
-    res.status(400);
-    res.json({
-        error: err.message,
-    });
+	console.error(err);
+	res.status(400);
+	res.json({
+		error: err.message
+	});
 });
 
 // Custom 404 (path not defined, or next() called without error)
 app.use((req, res) => {
-    res.status(404);
-    res.json({
-        error: 'not found'
-    });
-})
+	res.status(404);
+	res.json({
+		error: "not found"
+	});
+});
 
 // Set port and listen for incoming requests
 const port = 5000;
-app.listen(port, () => console.log('Server is listening on port', port));
+app.listen(port, () => console.log("Server is listening on port", port));
--- server/seed-data.sql
+++ server/seed-data.sql
@@ -1,5 +1,21 @@
 -- BASIC PHASE 1A - DROP and CREATE table
 -- Your code here
+DROP TABLE IF EXISTS trees;
+CREATE TABLE trees(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    tree VARCHAR(32),
+    location VARCHAR(64),
+    height_ft FLOAT,
+    ground_circumference_ft FLOAT
+);
 
 -- BASIC PHASE 1B - INSERT seed data
 -- Your code here
+
+INSERT INTO trees
+VALUES
+(null, 'General Sherman', 'Sequoia National Park', 274.9, 102.6),
+(null, 'General Grant', 'Kings Canyon National Park', 268.1, 107.5),
+(null, 'President', 'Sequoia National Park', 240.9, 93.0),
+(null, 'Lincoln', 'Sequoia National Park', 255.8, 98.3),
+(null, 'Stagg', 'Private Land', 243.0, 109.0);
